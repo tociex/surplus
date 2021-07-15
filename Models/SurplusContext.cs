ffe8a1b4a8dd2709999c31629999702c3fe581e0
tociex <wealthbisnis@gmail.com>
2021-07-15 09:21:52 +0700

    Penambahan Master Subtipe

--- Models/SurplusContext.cs
+++ Models/SurplusContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using tipe.Models;
 using uom.Models;
+using subtipe.Models;
 namespace surplus.Models{
     public class SurplusContext:DbContext
     {
@@ -11,6 +12,10 @@ namespace surplus.Models{
          public DbSet<Tipe> mastertipe{get; set;}
 
          public DbSet<Uom> masteruom{get; set;}
+
+         public DbSet<Subtipe> mastersubtipe{get; set;}
+
+        
     }
 
 }
